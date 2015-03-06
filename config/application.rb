166aa1d676095c4119d536d83585e82a21ee581a
subash poudyal <subashpoudyal@lftechnology.com>
2015-03-06 14:45:54 +0545

    factory girl default directory set

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,10 @@ module College
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.factory_girl :dir => 'spec/factories'
+    end
+
   end
 end
---
+++ spec/factories/college_registrations.rb
@@ -0,0 +1,6 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :college_registration do
+  end
+end
