47781ac44631b0eef08bed57618024517e56534d
subash poudyal <subashpoudyal@lftechnology.com>
2015-03-08 23:55:12 +0545

    update  and delete method for college registration controller spec

--- spec/controllers/college_registrations_controller_spec.rb
+++ spec/controllers/college_registrations_controller_spec.rb
@@ -90,61 +90,66 @@ RSpec.describe CollegeRegistrationsController, type: :controller do
       end
     end
   end
-  #
-  # describe "PUT update" do
-  #   describe "with valid params" do
-  #     let(:new_attributes) {
-  #       skip("Add a hash of attributes valid for your model")
-  #     }
-  #
-  #     it "updates the requested college_apply" do
-  #       college_apply = CollegeApply.create! valid_attributes
-  #       put :update, {:id => college_apply.to_param, :college_apply => new_attributes}, valid_session
-  #       college_apply.reload
-  #       skip("Add assertions for updated state")
-  #     end
-  #
-  #     it "assigns the requested college_apply as @college_apply" do
-  #       college_apply = CollegeApply.create! valid_attributes
-  #       put :update, {:id => college_apply.to_param, :college_apply => valid_attributes}, valid_session
-  #       expect(assigns(:college_apply)).to eq(college_apply)
-  #     end
-  #
-  #     it "redirects to the college_apply" do
-  #       college_apply = CollegeApply.create! valid_attributes
-  #       put :update, {:id => college_apply.to_param, :college_apply => valid_attributes}, valid_session
-  #       expect(response).to redirect_to(college_apply)
-  #     end
-  #   end
-  #
-  #   describe "with invalid params" do
-  #     it "assigns the college_apply as @college_apply" do
-  #       college_apply = CollegeApply.create! valid_attributes
-  #       put :update, {:id => college_apply.to_param, :college_apply => invalid_attributes}, valid_session
-  #       expect(assigns(:college_apply)).to eq(college_apply)
-  #     end
-  #
-  #     it "re-renders the 'edit' template" do
-  #       college_apply = CollegeApply.create! valid_attributes
-  #       put :update, {:id => college_apply.to_param, :college_apply => invalid_attributes}, valid_session
-  #       expect(response).to render_template("edit")
-  #     end
-  #   end
-  # end
-  #
-  # describe "DELETE destroy" do
-  #   it "destroys the requested college_apply" do
-  #     college_apply = CollegeApply.create! valid_attributes
-  #     expect {
-  #       delete :destroy, {:id => college_apply.to_param}, valid_session
-  #     }.to change(CollegeApply, :count).by(-1)
-  #   end
-  #
-  #   it "redirects to the college_applies list" do
-  #     college_apply = CollegeApply.create! valid_attributes
-  #     delete :destroy, {:id => college_apply.to_param}, valid_session
-  #     expect(response).to redirect_to(college_applies_url)
-  #   end
-  # end
+
+  describe "PUT update" do
+    describe "with valid params" do
+      let(:new_attributes) {
+        # skip("Add a hash of attributes valid for your model")
+        # FactoryGirl.attributes_for(:college_registration)
+        {
+         'name' => 'subash',
+         'description' => 'some description'
+        }
+      }
+
+      it "updates the requested college_registration" do
+        college_registration = CollegeRegistration.create! valid_attributes
+        put :update, { id: college_registration.to_param, :college_registration => new_attributes}, valid_session
+        college_registration.reload
+        # skip("Add assertions for updated state")
+      end
+
+      it "assigns the requested college_registration as @college_registration" do
+        college_registration = CollegeRegistration.create! valid_attributes
+        put :update, {:id => college_registration.to_param, :college_registration => valid_attributes}, valid_session
+        expect(assigns(:college_registration)).to eq(college_registration)
+      end
+
+      it "redirects to the college_registration" do
+        college_registration = CollegeRegistration.create! valid_attributes
+        put :update, {:id => college_registration.to_param, :college_registration => valid_attributes}, valid_session
+        expect(response).to redirect_to(college_registration)
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns the college_registration as @college_registration" do
+        college_registration = CollegeRegistration.create! valid_attributes
+        put :update, {:id => college_registration.to_param, :college_registration => invalid_attributes}, valid_session
+        expect(assigns(:college_registration)).to eq(college_registration)
+      end
+
+      it "re-renders the 'edit' template" do
+        college_registration = CollegeRegistration.create! valid_attributes
+        put :update, {:id => college_registration.to_param, :college_registration => invalid_attributes}, valid_session
+        expect(response).to render_template("edit")
+      end
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "destroys the requested college_registration" do
+      college_registration = CollegeRegistration.create! valid_attributes
+      expect {
+        delete :destroy, {:id => college_registration.to_param}, valid_session
+      }.to change(CollegeRegistration, :count).by(-1)
+    end
+
+    it "redirects to the college_registrations list" do
+      college_registration = CollegeRegistration.create! valid_attributes
+      delete :destroy, {:id => college_registration.to_param}, valid_session
+      expect(response).to redirect_to(college_registrations_url)
+    end
+  end
 
 end
